76cab3d4cf59e12f7eb06383d87b4b97f83969bf
timona <ronotimona@gmail.com>
2023-09-30 03:16:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Write your code here`
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
+//random 
+let random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+//mod 
+let num3 = 50
+let num4 = 23
+let mod = num3 % num4
 
-// Write your code here
+//max
+let max = Math.max(12,13,14,20,16,17,18,11)
+console.log(max)
